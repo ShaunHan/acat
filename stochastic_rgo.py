4ec5920ee081cabd8bb5109903b447e228be53be
Shuang Han <hanshuangshiren@gmail.com>
2020-11-28 16:08:56 +0000

    Replace stochastic_rgo.py

--- stochastic_rgo.py
+++ stochastic_rgo.py
@@ -1,9 +1,11 @@
-from cabins.adsorbate_coverage import *
-from cabins.adsorption_sites import *
+from allocat.adsorbate_coverage import *
+from allocat.adsorption_sites import *
 from ase.io import read, write, Trajectory
 from ase.calculators.emt import EMT
 from ase.optimize import BFGS, FIRE
 from ase.geometry import find_mic
+from ase.calculators.lammpslib import LAMMPSlib
+import gaussian_process as gp
 import networkx as nx
 import networkx.algorithms.isomorphism as iso
 import numpy as np
@@ -13,22 +15,39 @@ from ase.visualize import view
 
 
 # Read input trajectory of the previous generation
-structures = read('NiPt3_311_2_emt.traj', index=':')
+structures = read('NiPt3_311_1_reax.traj', index=':')
 # Write output trajectory of the current generation
-traj = Trajectory('NiPt3_311_3_emt.traj', mode='a')
+traj = Trajectory('NiPt3_311_2_reax.traj', mode='a')
 # Designate adsorbates
-monodentate_adsorbates = ['H','C','O','OH','CH','CO','H2O','CH2','COH','CH3','H2CO']
-bidentate_adsorbates = ['HCO','CHOH','CH2O','CH3O','H2COH','CH3OH']
+monodentate_adsorbates = ['H','C','O','OH','CH','CO','OH2','CH2','COH','CH3','OCH2','OCH3']
+bidentate_adsorbates = ['CHO','CHOH','CH2O','CH3O','CH2OH','CH3OH']
 adsorbates = monodentate_adsorbates + bidentate_adsorbates
 # Provide energy (eV) of the clean slab
-Eslab = 10.148632842226348
+Eslab = -325.32297835924203
+# Provide number of new structures generated in this generation
+Ngen = 200
+
+# Reaxff model
+model_dir = '/home/energy/shuha/project/4-NiPt_with_adsorbate'
+
+header=['units real',
+        'atom_style charge',
+        'atom_modify map array sort 0 0']
+
+cmds = ['pair_style reax/c NULL checkqeq yes safezone 1.6 mincap 100',
+        'pair_coeff * * {}/ffield_NiPt.reax H C O Ni Pt'.format(model_dir),
+        'fix 1 all qeq/reax 1 0.0 10.0 1.0e-6 reax/c']
+
+calc = LAMMPSlib(lmpcmds = cmds, lammps_header=header, 
+                 atom_types={'H':1,'C':2,'O':3,'Ni':4,'Pt':5}, 
+                 keep_alive=True, log_file='lammpslib.log')
 
 # Read AdsorptionSite object for the slab from pickle 
 with open('adsorption_sites_NiPt3_311.pkl', 'rb') as input:
     sas = pickle.load(input)
 
 Emin = min([struct.info['data']['Eads'] for struct in structures])
-Ecutoff = 2.25 # Set a very large cutoff for the 1st generation
+Ecutoff = 1.6 # Set a very large cutoff for the 1st generation
 starting_images = []
 old_labels_list = []
 old_graph_list = []
@@ -60,9 +79,9 @@ site_nblist = sas.get_neighbor_site_list()
 labels_list = []
 graph_list = []
 
-print(len(starting_images))
-random.shuffle(starting_images) 
-for image in starting_images: 
+Nnew = 0
+while Nnew < Ngen:
+    image = random.choice(starting_images) 
     sac = SlabAdsorbateCoverage(image, sas)
     fsl = sac.full_site_list
     nbstids = []
@@ -73,20 +92,24 @@ for image in starting_images:
                 selfids.append(j)
     nbsids = [v for v in nbstids if v not in selfids]
 
-    # Only add one adsorabte to a site at least 2 shells away from
-    # currently occupied sites
+    #TODO: select adsorbate with probablity weighed by performance
+    adsorbate = random.choice(adsorbates) 
     nsids = [i for i, s in enumerate(fsl) if i not in nbstids]
-    nsi = random.choice(nsids)
+    # Prohibit adsorbates with more than 1 atom from entering subsurf sites
+    subsurf_site = True
+    nsi = None
+    while subsurf_site: 
+        nsi = random.choice(nsids)
+        subsurf_site = (len(adsorbate) > 1 and fsl[nsi]['site'] == 'subsurf')
     nst = fsl[nsi]
     newnbs = site_nblist[nsi]
+    # Only add one adsorabte to a site at least 2 shells 
+    # away from currently occupied sites
     newnbids = [n for n in newnbs if n not in nbstids]
     if not newnbids:
         continue
 
     atoms = image.copy()
-    #TODO: select adsorbate with probablity weighed by performance
-    adsorbate = random.choice(adsorbates) 
-
     if adsorbate in bidentate_adsorbates:
     # Rotate a bidentate adsorbate to the direction of a randomly 
     # choosed neighbor site
@@ -118,6 +141,7 @@ for image in starting_images:
                                 if labels_list[i] == labs]
             if any(H for H in potential_graphs if 
             nx.isomorphism.is_isomorphic(G, H, node_match=nm)):
+                print('Duplicate found')
                 continue            
 
 #    # MC step
@@ -126,10 +150,12 @@ for image in starting_images:
     labels_list.append(labs)
     graph_list.append(G)
     
-    atoms.calc = EMT()                 
+    atoms.calc = calc                 
 #    opt = FIRE(atoms, logfile=None)
 #    opt.run(fmax=0.1)
+#    sas.update_positions(atoms)
     Etot = atoms.get_potential_energy()
     Eads = Etot - Eslab 
     atoms.info['data'] = {'Eads': Eads}
     traj.write(atoms)
+    Nnew += 1
