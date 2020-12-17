5fa2b6642cfe5d7580bb8c867e0dd4621dbd7a17
Shuang Han <hanshuangshiren@gmail.com>
2020-12-17 11:28:21 +0000

    Replace utilities.py

--- utilities.py
+++ utilities.py
@@ -1,9 +1,12 @@
 from ase.data import covalent_radii
 from ase.geometry import find_mic
 from collections import defaultdict
+from itertools import product, permutations
 import networkx as nx
 import numpy as np
 import scipy
+import math
+
 
 def neighbor_shell_list(atoms, dx=0.3, neighbor_number=1, 
                         different_species=False, mic=False,
@@ -29,30 +32,41 @@ def neighbor_shell_list(atoms, dx=0.3, neighbor_number=1,
         Whether to include all neighbors spanned within the shell
     """
 
-    if mic:
-        assert True in atoms.pbc    
     atoms = atoms.copy()
+    natoms = len(atoms)
     cell = atoms.cell
-    pbc = atoms.pbc
-    conn = {k: [] for k in range(len(atoms))}
+    nums = set(atoms.numbers)
+    pairs = product(nums, nums)
+    res = np.asarray(list(permutations(np.asarray(range(natoms)),2)))
+    indices1, indices2 = res[:,0], res[:,1]
+    positions = atoms.positions
+    p1, p2 = positions[indices1], positions[indices2]
+
+    if mic:
+        _, distances = find_mic(p2 - p1, cell, pbc=True)
+    else:
+        distances = np.linalg.norm(p2 - p1)
+    ds = np.insert(distances, np.arange(0, distances.size, natoms), 0.)
+
+    if not radius:
+        cr_dict = {(i, j): (covalent_radii[i]+covalent_radii[j]) for i, j in pairs}
+    
+    conn = {k: [] for k in range(natoms)}
     for atomi in atoms:
         for atomj in atoms:
-            if atomi.index != atomj.index:
+            i, j = atomi.index, atomj.index
+            if i != j:
                 if not (different_species & (atomi.symbol == atomj.symbol)):
-                    if mic:
-                        d = get_mic_distance(atomi.position,
-                                             atomj.position,
-                                             cell, pbc)
-                    else:
-                        d = np.linalg.norm(atomi.position - atomj.position)
+                    d = ds[i*natoms:(i+1)*natoms][j]
+                    crij = 2 * radius if radius else cr_dict[(atomi.number, atomj.number)] 
 
-                    cri = radius if radius else covalent_radii[atomi.number]
-                    crj = radius if radius else covalent_radii[atomj.number]
                     if neighbor_number == 1 or span:
                         d_max1 = 0.
                     else:
-                        d_max1 = ((neighbor_number - 1) * (crj + cri)) + dx
-                    d_max2 = (neighbor_number * (crj + cri)) + dx
+                        d_max1 = (neighbor_number - 1) * crij + dx
+
+                    d_max2 = neighbor_number * crij + dx
+
                     if d > d_max1 and d < d_max2:
                         conn[atomi.index].append(atomj.index)
 
@@ -75,14 +89,6 @@ def get_connectivity_matrix(neighborlist):
         conn_mat.append(conn_x)
 
     return np.asarray(conn_mat)
-
-
-def get_mic_distance(p1, p2, cell, pbc=True):
-    '''Calculate the distance using the minimum image convention'''
-
-    return find_mic(np.asarray([p1]) - np.asarray([p2]), 
-                    cell, pbc)[1][0]
-
 
 def expand_cell(atoms, cutoff=None, padding=None):
     """Return Cartesian coordinates atoms within a supercell
@@ -119,7 +125,7 @@ def expand_cell(atoms, cutoff=None, padding=None):
                                 [-1, 1, 1],
                                 [1, -1, 1],
                                 [-1, -1, 1]],
-                         cell)**2).sum(1))
+                                cell)**2).sum(1))
 
         if pos.shape[0] == 1:
             cutoff = max(diags) / 2.
@@ -136,10 +142,9 @@ def expand_cell(atoms, cutoff=None, padding=None):
     padding = pbc * np.array(np.ceil(cutoff * np.prod(latt_len) /
                                      (V * latt_len)), dtype=int)
 
-    offsets = np.mgrid[
-        -padding[0]:padding[0] + 1,
-        -padding[1]:padding[1] + 1,
-        -padding[2]:padding[2] + 1].T
+    offsets = np.mgrid[-padding[0]:padding[0] + 1,
+                       -padding[1]:padding[1] + 1,
+                       -padding[2]:padding[2] + 1].T
     tvecs = np.dot(offsets, cell)
     coords = pos[None, None, None, :, :] + tvecs[:, :, :, None, :]
 
@@ -150,7 +155,71 @@ def expand_cell(atoms, cutoff=None, padding=None):
 
     return index, coords, offsets
 
-def draw_graph(G, savefig=None):         
+
+def get_mic(p1, p2, cell, pbc=[1,1,0], max_cell_multiples=1e5, 
+            get_squared_distance=False): 
+    """
+    Get all vectors from p1 to p2 that are less than the cutoff in length
+    Also able to calculate the distance using the minimum image convention
+
+    This function is useful when you want to constantly calculate mic between 
+    two given positions. Please use ase.geometry.find_mic if you want to 
+    calculate an array of vectors all at a time (useful for e.g. neighborlist).    
+    
+    :param p1:
+    :param p2:
+    :param cutoff:
+    :param max_cell_multiples:
+    :return:
+    """
+    # Precompute some useful values
+    a, b, c = cell[0], cell[1], cell[2]
+    vol = np.abs(a @ np.cross(b, c))
+    a_cross_b = np.cross(a, b)
+    a_cross_b_len = np.linalg.norm(a_cross_b)
+    a_cross_b_hat = a_cross_b / a_cross_b_len
+    b_cross_c = np.cross(b, c)
+    b_cross_c_len = np.linalg.norm(b_cross_c)
+    b_cross_c_hat = b_cross_c / b_cross_c_len
+    a_cross_c = np.cross(a, c)
+    a_cross_c_len = np.linalg.norm(a_cross_c)
+    a_cross_c_hat = a_cross_c / a_cross_c_len
+
+    # TODO: Wrap p1, and p2 into the current unit cell
+    dr = p2 - p1
+    min_dr_sq = dr @ dr
+    min_length = math.sqrt(min_dr_sq)
+    a_max = math.ceil(min_length / vol * b_cross_c_len)
+    a_max = min(a_max, max_cell_multiples)
+    b_max = math.ceil(min_length / vol * a_cross_c_len)
+    b_max = min(b_max, max_cell_multiples)
+    if not pbc[2]:
+        c_max = 0
+    else:
+        c_max = math.ceil(min_length / vol * a_cross_b_len)
+        c_max = min(c_max, max_cell_multiples)
+
+    min_dr = dr
+    for i in range(-a_max, a_max + 1):
+        ra = i * a
+        for j in range(-b_max, b_max + 1):
+            rab = ra + j * b
+            for k in range(-c_max, c_max + 1):
+                if i == 0 and j == 0 and k == 0:
+                    continue
+                out_vec = rab + k * c + dr
+                len_sq = out_vec @ out_vec 
+                if len_sq < min_dr_sq:
+                    min_dr = out_vec
+                    min_dr_sq = len_sq
+    if not get_squared_distance:
+        return min_dr
+
+    else:
+        return np.sum(min_dr**2)
+
+
+def draw_graph(G, savefig=None):               
     import matplotlib.pyplot as plt
     labels = nx.get_node_attributes(G, 'label')
     nx.draw(G, labels=labels, font_size=8)
