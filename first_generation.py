9709f8354e62bf2c50c8125f677187241602a7bc
Shuang Han <hanshuangshiren@gmail.com>
2020-12-21 17:53:29 +0000

    Replace first_generation.py

--- first_generation.py
+++ first_generation.py
@@ -14,8 +14,8 @@ def main():
     traj = Trajectory('Ni3Pt_111_1_init.traj', mode='w')
 
     monodentate_adsorbates = ['H','C','O','OH','CH','CO','OH2','CH2','COH','CH3']#,'OCH2','OCH3']
-    bidentate_adsorbates = ['CHO']#,'CHOH','CH2O','CH3O','CH2OH','CH3OH']
-    adsorbates = monodentate_adsorbates + bidentate_adsorbates
+    multidentate_adsorbates = ['CHO']#,'CHOH','CH2O','CH3O','CH2OH','CH3OH']
+    adsorbates = monodentate_adsorbates + multidentate_adsorbates
 
 #    Eslab = slab.get_potential_energy() 
     with open(root + 'Ni3Pt_111_sites.pkl', 'rb') as input:
@@ -28,7 +28,7 @@ def main():
 
     for uni in unis:
         for adsorbate in monodentate_adsorbates:
-            if len(adsorbate) > 1 and uni[0] == 'subsurf':
+            if len(adsorbate) > 1 and uni[1] == 'subsurf':
                 continue
             atoms = slab.copy()    
             add_adsorbate(atoms, adsorbate,     
@@ -42,16 +42,16 @@ def main():
 #            sas.update_positions(atoms)
 #            Etot = atoms.get_potential_energy()
 #            Eads = Etot - Eslab            
-            sac = SlabAdsorbateCoverage(atoms, sas)
-            labels = sac.labels
-            atoms.info['data'] = {'labels': labels, 'Epot': None, 
-                                  'Gads_dft': None, 'Gads_krr': None}
+#            sac = SlabAdsorbateCoverage(atoms, sas)
+#            labels = sac.labels
+#            atoms.info['data'] = {'labels': labels, 'Epot': None, 
+#                                  'Gads_dft': None, 'Gads_krr': None}
             traj.write(atoms)
 
 
     for uni in unis:
-        for adsorbate in bidentate_adsorbates:
-            if uni[0] == 'subsurf':
+        for adsorbate in multidentate_adsorbates:
+            if uni[1] == 'subsurf':
                 continue
             atoms = slab.copy()
             si, site = next(((i, s) for i, s in enumerate(site_list) if 
@@ -70,10 +70,10 @@ def main():
 #            sas.update_positions(atoms)
 #            Etot = atoms.get_potential_energy()
 #            Eads = Etot - Eslab
-            sac = SlabAdsorbateCoverage(atoms, sas)
-            labels = sac.labels
-            atoms.info['data'] = {'labels': labels, 'Epot': None, 
-                                  'Gads_dft': None, 'Gads_krr': None}
+#            sac = SlabAdsorbateCoverage(atoms, sas)
+#            labels = sac.labels
+#            atoms.info['data'] = {'labels': labels, 'Epot': None, 
+#                                  'Gads_dft': None, 'Gads_krr': None}
             traj.write(atoms)
 
 if __name__ == "__main__":
