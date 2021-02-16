c385945619d3981dc8a0fa41fc584d14ae37cde0
Shuang Han <hanshuangshiren@gmail.com>
2021-02-16 13:09:44 +0000

    Upload New File

---
+++ data/physical_properties.py
@@ -0,0 +1,16 @@
+# Dictionaries for constructing fingerprints of machine learning models
+# Norskov
+dband_centers = {'H': 0., 'C': 0., 'O': 0., 'Ni': -1.29, 'Pt': -2.25,}
+
+# Wiki
+electron_affinities = {'H': 0.754, 'C': 1.262, 'O': 1.461, 'Ni': 1.157, 'Pt': 2.125,}
+
+# http://srdata.nist.gov/cccbdb/
+ionization_energies = {'H': 13.60, 'C': 11.26, 'O': 13.62, 'Ni': 7.64, 'Pt': 8.96,}
+
+# https://www.tandfonline.com/doi/full/10.1080/00268976.2018.1535143
+dipole_polarizabilities = {'H': 4.5, 'C': 11.3, 'O': 5.3, 'Ni': 49.0, 'Pt': 48.0,}
+
+# https://www.lenntech.com/periodic-chart-elements/electronegativity.htm
+electronegativities = {'H': 2.2, 'C': 2.55, 'O': 3.44, 'Ni': 1.9, 'Pt': 2.2,}
+
